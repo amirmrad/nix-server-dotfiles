e92277e9c4f07f0faba224158b42b925ed83ea37
Amir Rad <ahmr10@gmail.com>
2025-05-18 14:34:24 +0100

    add pubkey

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,7 @@
        isNormalUser = true;
        extraGroups = [ "wheel" ];
        openssh.authorizedKeys.keys = [
-         ssh-rsa <your_ssh_public_key>
+         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzv/7dMRN7F568vtxB3fZ1xBEuAjCNu4ajB1saPjcj4QWjjOKQpGXTtjhU9u/6DmPUQKkW4e2YznH9Al9qRBHvEsqaumcQymkCDrtSzd8XD5/uccLgId6XQMF84+H9XXY9tQ8ouDHkKZdHpE/7pLLB4/1zVyu45uIDmJczwRGP5+ZhqjaUdUkfCnVKLVL0Rzzs39bMmOC05g3FerXYWxP+9FOAzx1Y60lwDJv4noim66feEuOaAWd2jxZJm6bKw9voIgLwEd8VUPzBWg3wOiB1curKj+T29S3BwNnM+RcX35ll8SYRegUaumt/zJShVws0piNa45Kwx6VlDZsHmRyrJqDnpbpvbspdBZ1Z5eU+/Fb52wCmZ9rgD6f5WHdhU7xcTz2CP9Gis2kZUmbetV1K6zjwfxb7N9pwU0vyx0a3V00o3ka3YdlUW6ifva95W424UMuUZbSckTKW+nJTcT4jtZLXNFlAr8ezky1Y+Iqt6OHKxpIhtd8u5Dpi33WmVb8= amir@unity"
        ];
      };
    };
